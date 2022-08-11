5c6ddd089ed76b3ce94bccc4b1721c2420950468
LUCAS LIMA <10112971911@educacional.up>
2022-08-11 19:59:23 -0300

    Primeiro commit

---
+++ Aula/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a altura: ");
+
+
+            Console.WriteLine("Digite a largura: ");
+
+        }
+    }
+}
