c3b8c9e7dd8cc5f02c0aa455375b8d03dfe3a47f
LUCAS LIMA <10112971911@educacional.up>
2022-09-01 20:39:28 -0300

    Delete adicionado

--- FolhaPagamento/Controllers/FuncionarioController.cs
+++ FolhaPagamento/Controllers/FuncionarioController.cs
@@ -1,61 +1,93 @@
 using Microsoft.AspNetCore.Mvc;
 using FolhaPagamento.Models;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace FolhaPagamento.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FuncionarioController : ControllerBase
+    
+     [ApiController]
+     [Route("api/funcionario")]
+
+   public class FuncionarioController : ControllerBase
     {
-        //using System.Linq;
-        private static List<Funcionario> funcionarios = new List<Funcionario>();
 
-        //GET: /FolhaPagamento/funcionario/listar
-        [HttpGet]
-        [Route("listar")]
-        public IActionResult Listar()
-        {
-            return Ok(funcionarios);
-        }
 
-        //Post: /FolhaPagamento/funcionario/cadastrar
-        [HttpPost]
-        [Route("cadastrar")]
+    private static List<Funcionario> funcionarios = new List<Funcionario>();
 
-        public IActionResult Cadastrar(
-            [FromBody] Funcionario funcionario)
-        {
-            funcionarios.Add(funcionario);
-            return Created("", funcionario);
-        }
+    // GET: /api/funcionario/listar
+    [HttpGet]
+    [Route("Listar")]
+    public IActionResult Listar() => Ok(funcionarios);
 
-        //GET: /FolhaPagamento/usuario/buscar/{name}
-        [HttpGet]
-        [Route("buscar/{name}")]
-        public IActionResult Buscar([FromRoute] string name)
-        {
-            for (int i = 0; i < funcionarios.Count; i++)
-            {
-                if(funcionarios[i].Name.Equals(name)){
-                    return Ok(funcionarios[i]);
-                }
-            }
-            return NotFound();
-        }
+    // GET: /api/funcionario/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+   public IActionResult Cadastrar([FromBody] Funcionario funcionario)
+   {
+     
+     funcionarios.Add(funcionario);
+     return Created("",funcionario);
 
-        [HttpPut]
-        [Route("alterar/{name}")]
+   }
 
-        public async Task<ActionResult<List<Funcionario>>> Update(Funcionario request){
-            var funcionario = funcionarios.Find(i => i.Name == request.Name);
-            if (funcionarios == null)
-                return BadRequest("Funcionario nao encontrado");
+    // GET: /api/funcionario/buscar
+    [HttpGet]
+    [Route("buscar/{cpf}")]
+    public IActionResult Buscar([FromRoute] string cpf)
+    {
 
-                funcionario.Name = request.Name;
+        // Percorrer toda a lista da mesma maneira que o for abaixo porém 
+            // sem retornar o index de cada funcionario
 
-                return Ok(funcionarios);
-        }
+            // foreach(Funcionario funcionarioCadastrado in funcionarios){
+            //     if(funcionarioCadastrado.Cpf.Equals(cpf)){
+            //         return Ok(funcionarioCadastrado);
+            //     }
+            // }
+
+
+            // for (int i = 0; i < funcionarios.Count; i++)
+            // {
+            //     if(funcionarios[i].Cpf.Equals(cpf)){
+            //         return Ok(funcionarios[i]);
+            //     }
+            // }
+      
+      Funcionario funcionario = funcionarios.FirstOrDefault(
+      funcionarioCadastrado => funcionarioCadastrado.Cpf.Equals(cpf)
+      );
+         
+        // Expressão lambda
+     return funcionario != null ? Ok(funcionario) : NotFound();
+
+     // return funcionario != null ? Ok(funcionario) : NotFound();
+            // if(funcionario != null){
+            //    return Ok(funcionario);
+            // }
+            // return NotFound();
+
+     
+    } 
+
+    // DELETE: /api/funcionario/deletar/123
+    [Route("deletar/{cpf}")]
+    [HttpDelete]
+    public IActionResult Deletar([FromRoute] string cpf){
         
+        Funcionario funcionario = funcionarios.FirstOrDefault(
+            f => f.Cpf.Equals(cpf)
+        );
+        if(funcionario != null){
+            funcionarios.Remove(funcionario);
+            return Ok(funcionario);
+        }
+        return NotFound();
+        
+        
+        }
+    
     }
+
 }
--- FolhaPagamento/Models/Funcionario.cs
+++ FolhaPagamento/Models/Funcionario.cs
@@ -4,10 +4,13 @@ namespace FolhaPagamento.Models
 {
     public class Funcionario
     {
-        public Funcionario(){
-            CriadoEm = DateTime.Now;
-        }
-        public string Name {get; set;}
+        public Funcionario() => CriadoEm = DateTime.Now;
+        
+        public string Nome {get; set;}
+
+        public string Cpf {get; set;}
+
+        public DateTime Nascimento {get; set;}
 
         public DateTime CriadoEm {get; set;}
 
