8b4f180718bd567fa761e2f57c286ab81fab2d39
LUCAS LIMA <10112971911@educacional.up>
2022-09-22 19:23:15 -0300

    Inicio da Aula

---
+++ API_Folhas/Models/Funcionario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API_Folhas.Models
+{
+    public class Funcionario
+    {
+        public Funcionario() => CriadoEm = DateTime.Now;
+        public int FuncionarioId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime Nascimento { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
