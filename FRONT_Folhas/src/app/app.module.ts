54e6bd72a3ffef456451b7cc9b6bf7ee6cea38e9
LUCAS LIMA <10112971911@educacional.up>
2022-10-20 22:18:12 -0300

    Final da aula

--- FRONT_Folhas/src/app/app.module.ts
+++ FRONT_Folhas/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CadastrarFuncionarioComponent } from './components/pages/funcionario/folha/cadastrar-funcionario/cadastrar-funcionario.component';
+import { ListarFuncionarioComponent } from './components/pages/funcionario/folha/listar-funcionario/listar-funcionario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarFuncionarioComponent,
+    ListarFuncionarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
